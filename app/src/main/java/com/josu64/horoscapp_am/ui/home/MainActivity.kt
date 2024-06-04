0cecae7556dbebdae84bc9974bf97705dcb273b3
Josue Ibarra <josue.ibarra998@gmail.com>
2024-06-03 19:47:07 -0500

    Main bottom navigation bar connected to principal fragments

--- app/src/main/java/com/josu64/horoscapp_am/ui/home/MainActivity.kt
+++ app/src/main/java/com/josu64/horoscapp_am/ui/home/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.josu64.horoscapp_am.R
 import com.josu64.horoscapp_am.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +26,24 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets -> //If something happens, change binding.root for findViewById(R.id.main)
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0) //Bottom 0 because it was adding and extra margin to the bottom menu navigation
             insets
         }
+
+        initUI()
+
+    }
+
+    private fun initUI(){
+
+        initNavigation()
+
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment //As means type casting
+        navController = navHost.navController
+
+        binding.bottomNavMenu.setupWithNavController(navController)
     }
 }
